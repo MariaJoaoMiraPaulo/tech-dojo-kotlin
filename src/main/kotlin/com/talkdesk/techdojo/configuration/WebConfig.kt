0e99b151634b302a5d624fdc2eda47f83bc440db
Maria Paulo <maria.paulo@OPO-MBP-0113.local>
2019-09-26 11:19:03 +0100

    Deleted import.

--- src/main/kotlin/com/talkdesk/techdojo/configuration/WebConfig.kt
+++ src/main/kotlin/com/talkdesk/techdojo/configuration/WebConfig.kt
@@ -1,6 +1,6 @@
 package com.talkdesk.techdojo.configuration
 
-import org.graalvm.compiler.lir.alloc.trace.TraceGlobalMoveResolutionPhase.addMapping
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
